1ee15de801cb75af658660ea0e94107987122712
Thrasher08 <greenrupi@gmail.com>
2019-08-03 00:59:26 +0100

    Initial commit

---
+++ Monday Blues/Assets/Scripts/WindowDemoScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowDemoScript : MonoBehaviour
+{
+
+    public float defaultTime;
+    public float timer;
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        timer = defaultTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        timer = timer - Time.deltaTime;
+
+        if (timer <= 0.0f)
+        {
+            Debug.Log("Fail scrublord");
+        }
+    }
+
+    public void Reset()
+    {
+
+        timer = defaultTime;
+
+    }
+}
---
+++ Monday Blues/Assets/Scripts/WindowSwapper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowSwapper : MonoBehaviour
+{
+
+    bool toggle = true;
+
+    public Canvas Window1CanvasRef;
+    public Canvas Window2CanvasRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Window1CanvasRef.sortingOrder = 0;
+        Window2CanvasRef.sortingOrder = -1;
+        toggle = true;
+    }
+
+    // Update is called once per frame
+   public void Swap()
+    {
+        if (toggle == true)
+        {
+
+            Window1CanvasRef.sortingOrder = -1;
+            Window2CanvasRef.sortingOrder = 0;
+            toggle = false;
+
+        }
+        else
+        {
+
+            Window1CanvasRef.sortingOrder = 0;
+            Window2CanvasRef.sortingOrder = -1;
+            toggle = true;
+
+        }
+    }
+}
