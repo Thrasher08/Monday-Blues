04f0d3e2ec09516fac79bd9388637709b9d5c7eb
robotx211 <robert.brede97@gmail.com>
2019-08-03 12:39:00 +0100

    Commit before heading over

--- Monday Blues/Assets/Scripts/Window_Download.cs
+++ Monday Blues/Assets/Scripts/Window_Download.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Window_Download : MonoBehaviour
 {
+
+    public bool downloading;
+    public float downloadProgress;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        downloadProgress = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (downloading == true)
+        {
+
+
+
+        }
     }
 
     public void StartDownload()
