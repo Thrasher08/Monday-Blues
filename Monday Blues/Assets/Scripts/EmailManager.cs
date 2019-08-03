117e66be29f506e6f3785e5a3749f31eead8a4a1
Robert Brede <robert.brede97@gmail.com>
2019-08-03 20:50:04 +0100

    Added fail state for emails

--- Monday Blues/Assets/Scripts/EmailManager.cs
+++ Monday Blues/Assets/Scripts/EmailManager.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class EmailManager : MonoBehaviour
 {
+    public bool isRunning;
+
+    public float waitTime;
+    public float waitTimer;
+
+    public GameObject failedScreen;
+    public Text waitTimerText;
+
     [System.Serializable]
     public class Email
     {
@@ -22,6 +31,7 @@ public class EmailManager : MonoBehaviour
     public TextMeshProUGUI eMessage;
 
     public int eIndex;
+    public int lastEIndex;
 
     private void Start()
     {
@@ -29,11 +39,19 @@ public class EmailManager : MonoBehaviour
         eName.text = emails[eIndex].name;
         eSubject.text = emails[eIndex].subject;
         eMessage.text = emails[eIndex].message;
+
+        failedScreen.SetActive(false);
     }
 
     public void eRandomiser()
     {
-        eIndex = Random.Range(0, 6);
+        int i = 0;
+        while (eIndex == lastEIndex && i < 10)
+        {
+            eIndex = Random.Range(0, 6);
+            i++;
+        }
+                
     }
 
     public void confirmEmail()
@@ -41,9 +59,11 @@ public class EmailManager : MonoBehaviour
         if (emails[eIndex].confirm)
         {
             Debug.Log("CORRECT CONFIRM");
+            NextEmail();
         } else
         {
             Debug.Log("INCORRECT CONFIRM");
+            FailedEmail();
         }
     }
 
@@ -52,10 +72,52 @@ public class EmailManager : MonoBehaviour
         if (!emails[eIndex].confirm)
         {
             Debug.Log("CORRECT DENY");
+            NextEmail();
         }
         else
         {
             Debug.Log("INCORRECT DENY");
+            FailedEmail();
+        }
+    }
+
+    public void NextEmail ()
+    {
+        lastEIndex = eIndex;
+        eRandomiser();
+        eName.text = emails[eIndex].name;
+        eSubject.text = emails[eIndex].subject;
+        eMessage.text = emails[eIndex].message;
+    }
+
+    public void FailedEmail ()
+    {
+        isRunning = false;
+
+        failedScreen.SetActive(true);
+
+        waitTimer = waitTime;
+    }
+
+    void Update()
+    {
+        if (isRunning == false)
+        {
+
+            waitTimer = waitTimer - Time.deltaTime;
+            waitTimerText.text = Mathf.CeilToInt(waitTimer).ToString() + "s Left!";
+
+            if (waitTimer <= 0.0f)
+            {
+
+
+                isRunning = true;
+
+                NextEmail();
+
+                failedScreen.SetActive(false);
+
+            }
         }
     }
 }
