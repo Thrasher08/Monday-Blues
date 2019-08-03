7db7f18e6c64cca29bb06fc6850b2fdc1130712f
Thrasher08 <greenrupi@gmail.com>
2019-08-03 19:19:07 +0100

    Email & Car Window Update

---
+++ Monday Blues/Assets/Scripts/EmailManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EmailManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Email
+    {
+        public string name;
+        public string subject;
+        public string message;
+
+        public bool confirm;
+    }
+
+    public Email[] emails;
+
+    public TextMeshProUGUI eName;
+    public TextMeshProUGUI eSubject;
+    public TextMeshProUGUI eMessage;
+
+    public int eIndex;
+
+    private void Start()
+    {
+        eRandomiser();
+        eName.text = emails[eIndex].name;
+        eSubject.text = emails[eIndex].subject;
+        eMessage.text = emails[eIndex].message;
+    }
+
+    public void eRandomiser()
+    {
+        eIndex = Random.Range(0, 6);
+    }
+
+    public void confirmEmail()
+    {
+        if (emails[eIndex].confirm)
+        {
+            Debug.Log("CORRECT CONFIRM");
+        } else
+        {
+            Debug.Log("INCORRECT CONFIRM");
+        }
+    }
+
+    public void denyEmail()
+    {
+        if (!emails[eIndex].confirm)
+        {
+            Debug.Log("CORRECT DENY");
+        }
+        else
+        {
+            Debug.Log("INCORRECT DENY");
+        }
+    }
+}
---
+++ Monday Blues/Assets/Scripts/Typing.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Typing : MonoBehaviour
+{
+    // Start is called before the first frame update#
+
+    public string response;
+    public TextMeshProUGUI inputResponse;
+    public float speed = 0.05f;
+    int rLength;
+    int index;
+    public bool ableToType;
+
+
+    void Start()
+    {
+        index = 0;
+        rLength = response.Length;
+        ableToType = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ableToType)
+        {
+            if (Input.anyKeyDown)
+            {
+                typeMessage(response);
+                index++;
+
+                if (index > rLength)
+                {
+                    Debug.Log("HI");
+                }
+            }
+        }
+
+    }
+
+    public void typeMessage(string response)
+    {
+        Debug.Log(response + index);
+        inputResponse.text += response[index];
+    }
+
+    /*private IEnumerator AnimateTypingCoroutine(string response)
+    {
+        for (int i = 0; i < response.Length; i++)
+        {
+            inputResponse.text += response[i];
+            i++;
+        }
+
+        yield return new WaitForSeconds(speed);
+    }*/
+}
