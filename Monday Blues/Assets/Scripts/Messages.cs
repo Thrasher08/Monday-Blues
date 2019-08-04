a5feb1b1bc2ee47352183d80210ddfc8de9833f6
Robert Brede <robert.brede97@gmail.com>
2019-08-04 18:14:29 +0100

    Finished messenger window functionality

--- Monday Blues/Assets/Scripts/Messages.cs
+++ Monday Blues/Assets/Scripts/Messages.cs
@@ -2,36 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Messages : MonoBehaviour
 {
     public TextMeshProUGUI userResponse;
-    public TextMeshProUGUI[] messageOrder;
+    public List<TextMeshProUGUI> messageOrder;
+
+    public GameObject messageContainer;
+    public GameObject defaultTextObject;
+
     public string[] messages;
     public int mIndex;
     public int lastMIndex;
 
-    float timer;
-    float speed = 0.05f;
+    public float messageTypingSpeed = 0.05f;
+
+    public bool waitingForNewMessage;
+    public float newMessageTime;
+    public float newMessageTimer;
+
+    public bool waitingForResponse;
+    public float responseTime;
+    public float responseTimer;
+
+    public bool isLockedOut;
+    public float lockoutTime;
+    public float lockoutTimer;
+
+    public Typing typingScript;
+
+    public GameObject failedScreen;
+    public Text waitTimerText;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        userResponse.text = "";
+        foreach (TextMeshProUGUI textbox in messageOrder)
+        {
+            textbox.text = "";
+        }
+
+        lastMIndex = -1;
         mRandomiser();
-        messageOrder[0].text = messages[mIndex];
         timeMessage();
-        timer = 0;
+
+        waitingForResponse = true;
+        waitingForNewMessage = false;
+        isLockedOut = false;
+
+        failedScreen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if(timer > 3f)
+        if (waitingForResponse == true)
         {
-            timeMessage();
-            timer = 0;
+
+            responseTimer = responseTimer + Time.deltaTime;
+
+            if (responseTimer > responseTime)
+            {
+                ResponseMissed();
+            }
+
+        }
+        else if (waitingForNewMessage == true)
+        {
+            newMessageTimer = newMessageTimer + Time.deltaTime;
+
+            if (newMessageTimer > newMessageTime)
+            {
+                timeMessage();
+            }
+        }
+        else if (isLockedOut == true)
+        {
+            lockoutTimer = lockoutTimer - Time.deltaTime;
+
+            waitTimerText.text = Mathf.CeilToInt(lockoutTimer).ToString() + "s Left!";
+
+            if (lockoutTimer <= 0)
+            {
+                isLockedOut = false;
+                waitingForNewMessage = true;
+
+                newMessageTimer = 0;
+                responseTimer = 0;
+
+                failedScreen.SetActive(false);
+            }
         }
 
     }
@@ -41,55 +104,153 @@ public class Messages : MonoBehaviour
         int i = 0;
         while (mIndex == lastMIndex && i < 10)
         {
-            mIndex = Random.Range(0, 8);
+            mIndex = Random.Range(0, messages.Length);
             i++;
         }
 
     }
 
-    public void submitResponse()
+    public void PushUpMessages ()
     {
-        messageOrder[5].text = messageOrder[4].text;
-        messageOrder[4].text = messageOrder[3].text;
-        messageOrder[3].text = messageOrder[2].text;
-        messageOrder[2].text = messageOrder[1].text;
-        messageOrder[1].text = messageOrder[0].text;
-        messageOrder[0].text = userResponse.text;
+        GameObject temp = messageOrder[0].gameObject;
+        messageOrder.RemoveAt(0);
+        Destroy(temp); //destroy top text object
+
+        GameObject newTMP = Instantiate(defaultTextObject, messageContainer.transform);
+        messageOrder.Add(newTMP.GetComponent<TextMeshProUGUI>());
+
+    }
+
+    public void trySubmitResponse()
+    {
+        if (waitingForResponse == false)
+        {
+            Debug.LogWarning("No Response Needed Yet");
+            return;
+        }
+
+        if (typingScript.isResponseReady == false)
+        {
+            Debug.LogWarning("Response is not Ready Yet");
+            return;
+        }
+
+        Debug.Log("Submitted Response");
+
+        PushUpMessages();
+
+        messageOrder[messageOrder.Count - 1].text = userResponse.text;
+        messageOrder[messageOrder.Count - 1].alignment = TextAlignmentOptions.Right;
 
         userResponse.text = "";
+
+        waitingForNewMessage = true;
+        waitingForResponse = false;
+
+        responseTimer = 0.0f;
+        newMessageTimer = 0.0f;
+
+        typingScript.clearResponse();
+
+        ResponseSuccessful();
+    }
+
+    public void submitFailedResponse ()
+    {
+        PushUpMessages();
+
+        messageOrder[messageOrder.Count - 1].text = "[This User Failed To Respond To Your Message]";
+        messageOrder[messageOrder.Count - 1].alignment = TextAlignmentOptions.Right;
+
     }
 
     public void timeMessage()
     {
-        messageOrder[5].text = messageOrder[4].text;
-        messageOrder[4].text = messageOrder[3].text;
-        messageOrder[3].text = messageOrder[2].text;
-        messageOrder[2].text = messageOrder[1].text;
-        messageOrder[1].text = messageOrder[0].text;
-        AnimateDialogueBox(messages[mIndex]);           //NEEDS TO BE AUTOMATED OVERTIME
+        Debug.Log("New Message");
+
+        PushUpMessages();
+
+        AnimateDialogueBox(messages[mIndex], messageOrder[messageOrder.Count - 1]);
+
+        waitingForNewMessage = false;
+        waitingForResponse = true;
+
+        responseTimer = 0.0f;
+        newMessageTimer = 0.0f;
+
+        typingScript.clearResponse();
     }
 
-    public void AnimateDialogueBox(string dialogue)
+    public void AnimateDialogueBox(string dialogue, TextMeshProUGUI textBox)
     {
-        Debug.Log("Work please");
+        if (textBox == null)
+        {
+            Debug.LogError("Non-Existat TextBox");
+            return;
+        }
+
+        //Debug.Log("Work please");
         lastMIndex = mIndex;
         mRandomiser();
-        StartCoroutine(AnimateTextCoroutine(dialogue));
+        StartCoroutine(AnimateTextCoroutine(dialogue, textBox));
     }
 
-    private IEnumerator AnimateTextCoroutine(string dialogue)
+    private IEnumerator AnimateTextCoroutine(string dialogue, TextMeshProUGUI textBox)
     {
-        messageOrder[0].text = "";
+        textBox.text = "";
         int i = 0;
 
         while (i < dialogue.Length)
         {
-            messageOrder[0].text += dialogue[i];
+            if (textBox == null)
+            {
+                Debug.LogWarning("Non-Existat TextBox");
+                break;
+            }
+
+            textBox.text += dialogue[i];
 
             i++;
 
-            yield return new WaitForSeconds(speed);
+            yield return new WaitForSeconds(messageTypingSpeed);
         }
+        yield return null;
+    }
+
+    public void ResponseMissed()
+    {
+        //lockout
+        //wait for new message
+
+        Debug.Log("Response Missed");
+
+        submitFailedResponse();
+        
+
+        lockoutTimer = lockoutTime;
+
+        isLockedOut = true;
+        waitingForNewMessage = false;
+        waitingForResponse = false;
+
+        failedScreen.SetActive(true);
+
+        typingScript.clearResponse();
+
+    }
+
+    public void ResponseSuccessful()
+    {
+        //wait for new message
+        Debug.Log("Response Successful");
+
+        waitingForNewMessage = true;
+        waitingForResponse = false;
+
+        responseTimer = 0;
+        newMessageTimer = 0;
+
+        typingScript.clearResponse();
     }
 
 }
--- Monday Blues/Assets/Scripts/Typing.cs
+++ Monday Blues/Assets/Scripts/Typing.cs
@@ -10,43 +10,79 @@ public class Typing : MonoBehaviour
 
     public string response;
     public TextMeshProUGUI inputResponse;
+    public TextMeshProUGUI responseShadow;
     public float speed = 0.05f;
     int rLength;
     int index;
-    public bool ableToType;
 
+    public bool isResponseReady;
+
+    public Messages messagesScript;
 
     void Start()
     {
         index = 0;
         rLength = response.Length;
-        ableToType = true;
+        isResponseReady = false;
+
+        inputResponse.text = "";
+        responseShadow.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(ableToType);
-        if (ableToType)
+        //Debug.Log(ableToType);
+        if (messagesScript.waitingForResponse == true)
         {
+            if (responseShadow.text != response)
+            {
+                responseShadow.text = response;
+            }
+
             if (Input.anyKeyDown)
             {
-                typeMessage(response);
-                index++;
-
-                if (index > rLength)
+                if (Input.GetMouseButtonDown(0)
+                 || Input.GetMouseButtonDown(1)
+                 || Input.GetMouseButtonDown(2))
                 {
-                    Debug.Log("HI");
+                    return;
+                }
+
+                if (index > rLength - 1)
+                {
+                    Debug.Log("Finished Word");
+                }
+                else
+                {
+                    typeMessage(response);
+                    index++;
                 }
             }
+        }
+
+        if (inputResponse.text == response)
+        {
+            isResponseReady = true;
+        }
+        else
+        {
+            isResponseReady = false;
         }
 
     }
 
     public void typeMessage(string response)
     {
-        Debug.Log(response + index);
+        //Debug.Log(response + index);
         inputResponse.text += response[index];
+    }
+
+    public void clearResponse ()
+    {
+        index = 0;
+        inputResponse.text = "";
+        responseShadow.text = "";
     }
 
     /*private IEnumerator AnimateTypingCoroutine(string response)
