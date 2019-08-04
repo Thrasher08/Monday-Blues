2a6115683ce2a6cc66f693943278fd2131c11618
robotx211 <robert.brede97@gmail.com>
2019-08-04 03:00:00 +0100

    Added taskbar icons and car sprites

---
+++ Monday Blues/Assets/Scripts/CarGame/EnemyCarSkinPicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCarSkinPicker : MonoBehaviour
+{
+
+    public List<Sprite> carSprites;
+
+    public Image carImage;
+
+    void Awake()
+    {
+        if (carSprites.Count == 0)
+        {
+            return;
+        }
+
+        int index = Random.Range(0, carSprites.Count);
+        carImage.sprite = carSprites[index];
+    }
+
+}
